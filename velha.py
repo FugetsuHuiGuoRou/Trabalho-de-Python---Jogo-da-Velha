92070fc20f3d41124e5ba3187a838f2a3ca6ae19
FugetsuHuiGuoRou <cassandrapascal2010@gmail.com>
2026-04-14 13:49:32 -0300

    feat: função def

---
+++ velha.py
@@ -0,0 +1,25 @@
+
+#Primeiro, defini crie uma lista para representar o tabuleiro do jogo.
+tabuleiro = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+# Segundo, defini uma função para exibir o tabuleiro do jogo. 
+# Essa função irá imprimir o estado atual do tabuleiro na tela, 
+# mostrando as posições ocupadas pelos jogadores e as posições vazias.
+
+#Quando você chamar a função feche com () para deixar claro que é uma função, e não uma variável.
+# Dois pontos : serve para indicar o início do bloco de código que pertence à função.
+
+def exibir_tabuleiro():
+    print("┌───┬───┬───┐")
+    print(f"│ {tabuleiro[0]} │ {tabuleiro[1]} │ {tabuleiro[2]} │")
+    print("├───┼───┼───┤")
+    print(f"│ {tabuleiro[3]} │ {tabuleiro[4]} │ {tabuleiro[5]} │")
+    print("├───┼───┼───┤")
+    print(f"│ {tabuleiro[6]} │ {tabuleiro[7]} │ {tabuleiro[8]} │")
+    print("└───┴───┴───┘")
+
+
+
+
+
