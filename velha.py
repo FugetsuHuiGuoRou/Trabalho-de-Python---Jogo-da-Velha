26af3521de61c49308343046aa9668c8b8ce5ca1
FugetsuHuiGuoRou <cassandrapascal2010@gmail.com>
2026-04-14 16:25:40 -0300

    feat: while true

--- velha.py
+++ velha.py
@@ -1,5 +1,5 @@
 
-#Primeiro, defini crie uma lista para representar o tabuleiro do jogo.
+#Primeiro,  criei ma lista para representar o tabuleiro do jogo.
 tabuleiro = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 
@@ -12,13 +12,27 @@ tabuleiro = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def exibir_tabuleiro():
     print("┌───┬───┬───┐")
-    print(f"│ {tabuleiro[0]} │ {tabuleiro[1]} │ {tabuleiro[2]} │")
+    print(f"│ {tabuleiro[0]} │ {tabuleiro[1]} │ {tabuleiro[2]} │") 
     print("├───┼───┼───┤")
     print(f"│ {tabuleiro[3]} │ {tabuleiro[4]} │ {tabuleiro[5]} │")
     print("├───┼───┼───┤")
     print(f"│ {tabuleiro[6]} │ {tabuleiro[7]} │ {tabuleiro[8]} │")
     print("└───┴───┴───┘")
+    #Tudo o que está entre chaves {} é uma expressão que será avaliada e seu resultado será convertido em string e inserido no lugar das chaves.
 
+
+
+jogador_atual = "X"
+
+while True:
+    exibir_tabuleiro()  # Chama sua função para mostrar o jogo
+    print(f"Vez do jogador: {jogador_atual}")
+    
+    # Recebe a jogada do usuário
+    jogada = int(input("Escolha uma posição (1-9)"))  
+    
+    # Diminui 1 da jogada para ajustar ao índice da lista (0-8) Pro jogador não precisar digitar 0.
+    tabuleiro[jogada - 1] = jogador_atual
 
 
 
